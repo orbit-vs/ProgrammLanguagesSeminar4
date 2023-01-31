33b67dfb4096760984cea655340fea99c0774bac
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-31 19:14:33 +0300

    Seminar4.3

---
+++ Seminar4.3/Program.cs
@@ -0,0 +1,23 @@
+﻿int [] EnterArray(int N)
+{
+    int [] array = new int[N];
+    int len = array.Length;
+    for (int i = 0; i < len; i++)
+{
+    array[i] = new Random().Next(0, 2);
+} 
+return array;
+
+ }
+
+void PrintArray(int[] array)
+{
+    int len = array.Length;
+System.Console.Write("[");
+for (int i = 0; i < len; i++)
+
+{if (i == len -1)
+   System.Console.Write($"{array[i]}]");
+else System.Console.Write($"{array[i]},");   
+}
+}
