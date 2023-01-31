ba47b5b66a7dbd5a017dff558a9552c5bd13d754
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-31 19:13:13 +0300

    Seminar4

---
+++ Seminar4/Program.cs
@@ -0,0 +1,33 @@
+﻿int SumFromOneToN(int n)
+{
+
+    if (n < 1)
+        return -1; 
+    
+    int result = 0;
+
+    int counter = 1;
+
+    while(counter <= n) {
+        result += counter;
+        counter ++;
+    }
+    
+    return result;
+}
+
+
+Console.WriteLine("Программа вычесления суммы чисел от 1 до N");
+Console.WriteLine("Введите число");
+int.TryParse(Console.ReadLine(), out int x);
+
+
+
+int result = SumFromOneToN(n);
+
+int SumFromOneToN(object n)
+{
+    throw new NotImplementedException();
+}
+
+Console.WriteLine($"Результат: {result}");
