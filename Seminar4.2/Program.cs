31f73b74bdc3924448318c4ff54099258c9a071f
Артем Белоколенко <artembelokolenko@MacBook-Air-Artem.local>
2023-01-31 19:14:16 +0300

    Seminar4.2

---
+++ Seminar4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿int N = getVar();
+System.Console.WriteLine( multipler(N) );
+
+int multipler(int num) {
+    int outNum = 1;
+    for (int i = 1; i < num + 1; outNum *= i++){}
+
+    return outNum;
+}
+
+int getVar(){
+    int varValue = 0;
+    bool isNumeric = false;
+
+    while (!isNumeric) {
+        Console.WriteLine($"Введите число: ");
+        isNumeric = int.TryParse(Console.ReadLine(), out varValue);
+    }
+
+    return varValue;
+}
